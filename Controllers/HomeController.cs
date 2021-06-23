2e4dda01873dbb1b623cb21752e17e49238938be
hungvx <hungvx@bytesoft.net>
2021-06-23 15:32:45 +0700

    hungvx commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebZipIt.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public HomeController(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task DownloadBots()
+        {
+            Response.ContentType = "application/octet-stream";
+            Response.Headers.Add("Content-Disposition", "attachment; filename=\"Bots.zip\"");
+
+            var botsFolderPath = Path.Combine(hostEnvironment.ContentRootPath, "bots");
+            var botFilePaths = Directory.GetFiles(botsFolderPath);
+            using (ZipArchive archive = new ZipArchive(Response.BodyWriter.AsStream(), ZipArchiveMode.Create))
+            {
+                foreach (var botFilePath in botFilePaths)
+                {
+                    var botFileName = Path.GetFileName(botFilePath);
+                    var entry = archive.CreateEntry(botFileName);
+                    using (var entryStream = entry.Open())
+                    using (var fileStream = System.IO.File.OpenRead(botFilePath))
+                    {
+                        await fileStream.CopyToAsync(entryStream);
+                    }
+                }
+            }
+        }
+
+        public async Task<IActionResult> DownloadBotsWithMemoryStream()
+        {
+            var botsFolderPath = Path.Combine(hostEnvironment.ContentRootPath, "bots");
+            var botFilePaths = Directory.GetFiles(botsFolderPath);
+            var zipFileMemoryStream = new MemoryStream();
+            using (ZipArchive archive = new ZipArchive(zipFileMemoryStream, ZipArchiveMode.Update, leaveOpen: true))
+            {
+                foreach (var botFilePath in botFilePaths)
+                {
+                    var botFileName = Path.GetFileName(botFilePath);
+                    var entry = archive.CreateEntry(botFileName);
+                    using (var entryStream = entry.Open())
+                    using (var fileStream = System.IO.File.OpenRead(botFilePath))
+                    {
+                        await fileStream.CopyToAsync(entryStream);
+                    }
+                }
+            }
+
+            zipFileMemoryStream.Seek(0, SeekOrigin.Begin);
+            return File(zipFileMemoryStream, "application/octet-stream", "Bots.zip");
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebZipIt.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public IndexModel(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnGetDownloadBots()
+        {
+            Response.ContentType = "application/octet-stream";
+            Response.Headers.Add("Content-Disposition", "attachment; filename=\"Bots.zip\"");
+
+            var botsFolderPath = Path.Combine(hostEnvironment.ContentRootPath, "bots");
+            var botFilePaths = Directory.GetFiles(botsFolderPath);
+            using (ZipArchive archive = new ZipArchive(Response.BodyWriter.AsStream(), ZipArchiveMode.Create))
+            {
+                foreach (var botFilePath in botFilePaths)
+                {
+                    var botFileName = Path.GetFileName(botFilePath);
+                    var entry = archive.CreateEntry(botFileName);
+                    using (var entryStream = entry.Open())
+                    using (var fileStream = System.IO.File.OpenRead(botFilePath))
+                    {
+                        await fileStream.CopyToAsync(entryStream);
+                    }
+                }
+            }
+            
+            return new EmptyResult();
+        }
+    }
+}
